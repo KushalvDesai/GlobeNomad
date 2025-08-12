88df132d70aa2a6b3b9ea497f37f5693f54632c8
Kushal Desai <139681085+KushalvDesai@users.noreply.github.com>
2025-08-12 08:58:46 +0530

    Delete old be directory

--- be/src/cost-estimator/cost-estimator.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CostEstimatorService } from './cost-estimator.service';
-import { CostEstimatorResolver } from './cost-estimator.resolver';
-
-@Module({
-  providers: [CostEstimatorService, CostEstimatorResolver],
-})
-export class CostEstimatorModule {}
